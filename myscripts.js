cf865057d8645ba441af5391e21a28d7854c4b59
Beshir Saeed <beshir-saeed@live.com>
2022-05-13 03:34:07 +0300

    Complete JS functions for requirements

--- myscripts.js
+++ myscripts.js
@@ -1,15 +1,17 @@
 const plays = ["rock", "paper", "scissors"];
+let playerSelection, computerSelection;
 
+//Chooses a random option between Rock, Paper and Scissors
 function computerPlay() {
     return plays[Math.floor(Math.random()*3)];
 }
 
+//Completes a round of Rock-Paper-Scissors between the player and the computer
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
-
-    if (!playerSelection in plays) {
-        return console.error("Invalid Entry");
-    }
+    computerSelection = computerPlay();
+    do {
+        playerSelection = prompt("Choose between: Rock, Paper, Scissors").toLowerCase();
+    } while (!plays.includes(playerSelection));
 
     if (playerSelection == computerSelection) {
         return "You tied!"
@@ -32,7 +34,29 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-let computerSelection = computerPlay();
-let playerSelection = "rock";
+function game() {
+    let roundResult = "";
+    let computerWinCount = 0;
+    let playerWinCount = 0;
 
-console.log(playRound(playerSelection, computerSelection));
+    for(let i = 0;i < 5; i++) {
+        roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult[4] == "W");
+        if (roundResult[4] == "W") {
+            playerWinCount++;
+        } else if (roundResult[4] == "L") {
+            computerWinCount++;
+        } else 
+    
+        console.log(roundResult);
+    }
+    if (playerWinCount > computerWinCount) {
+        console.log(`Congratz! You Won!`);
+    } else if(computerWinCount > playerWinCount) {
+        console.log(`You lost, Hard luck`);
+    } else {
+        console.log(`The game was a Tie`);
+    }
+}
+
+game();
